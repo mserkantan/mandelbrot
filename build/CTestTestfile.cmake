6d8ca1a01dd7de32c623789f502af6e79985d5fb
serkan tan <m.serkantan@gmail.com>
2021-10-18 22:03:23 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/serkan/mandelbrot/src
-# Build directory: /home/serkan/mandelbrot/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("main")
--- build/main/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/serkan/mandelbrot/src/main
-# Build directory: /home/serkan/mandelbrot/build/main
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
